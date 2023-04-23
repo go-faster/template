ff5ab48a475ed0b489c811b55b08d66dae8d67e5
Aleksandr Razumov <ernado@go-faster.org>
2023-04-23 07:54:31 +0300

    feat: template.sh

--- template.sh
+++ template.sh
@@ -4,7 +4,8 @@ set -e
 set -x
 
 template="github.com/go-faster/template"
+project="github.com/go-faster/${PWD##*/}"
 commit="$(git rev-list --max-parents=0 HEAD)"
 
-grep -rl "${template}" . | xargs sed -i "s|${template}|newtext|g"
+grep -rl "${template}" . | xargs sed -i "s|${template}|${project}|g"
 grep -rl "bca36d71977c623525a48c631309fcedb26605a9" . | xargs sed -i "s|bca36d71977c623525a48c631309fcedb26605a9|${commit}|g"
