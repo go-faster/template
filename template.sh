37eacfc2a7af09ee10ee3365703d3522f7a08e58
Aleksandr Razumov <ernado@go-faster.org>
2023-04-23 08:02:50 +0300

    fix: add pkg.go

--- template.sh
+++ template.sh
@@ -4,9 +4,15 @@ set -e
 set -x
 
 template="github.com/go-faster/template"
-project="github.com/go-faster/${PWD##*/}"
+name="${PWD##*/}"
+project="github.com/go-faster/${name}"
 head="$(git rev-list --max-parents=0 HEAD)"
 commit="a65086111716ab748f562ce12cef7d8c01f91c92"
 
 grep -rl "${template}" . | xargs sed -i "s|${template}|${project}|g"
 grep -rl "${commit}" . | xargs sed -i "s|${commit}|${head}|g"
+
+rm pkg.go
+echo "package ${name}" > "${name}".go
+
+rm template.sh
